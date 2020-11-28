94b85ca9520e891b779c1ac93f4bd4221b9b132a
katplee <62573178+katplee@users.noreply.github.com>
2020-11-28 21:06:22 +0900

    Initial file backup
    Polished code to make execution more efficient. Used delegates and events to achieve "laggy, 8-bit, retro game" effect. Called the method ModifiedUpdate to represent an "update method" whose update rate can be declared by the user.
    
    Also added progress bar until the current destination at the HUD.
    
    tags: slider, events and delegates, "modified update"

---
+++ Moonshot/Game Off 2020/Assets/Scripts/UI/SliderPrefab.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderPrefab : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private Gradient gradient;
+    [SerializeField]
+    private Image fillImage;
+
+    void Start()
+    {
+        SetStartingPosition(10f);
+    }
+
+    public void SetStartingPosition(float maxDistance)
+    {
+        slider.maxValue = maxDistance;
+        slider.value = 0f;
+        //fillImage.color = gradient.Evaluate(0f);
+    }
+
+    public void SetCurrentPosition(float currentDistance)
+    {
+        slider.value = currentDistance;
+        //fillImage.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
